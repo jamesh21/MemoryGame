1c1a7ccf9ef07e0572a14265b4ddc8bdb8eeedb1
jamesh21 <jamesh21@uw.edu>
2018-05-08 01:10:20 -0700

    Add randomized icons to cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+let list = ['android', 'react', 'java', 'node-js', 'node-js', 'react', 'html5',
+            'css3-alt', 'css3-alt', 'html5', 'android', 'python', 'python', 'java',
+            'github', 'github'];
+const cardContainer = document.querySelector('.card-container');
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// function is used to place items in the list to their respective cards
+function placeCards(element, index) {
+  const cardIcon = cardContainer.children[index].children[0];
+  cardIcon.classList.add(`fa-${element}`);
+}
+
+// function addCardListeners() {
+//
+// }
+
+// shuffle the starting list
+list = shuffle(list);
+list.forEach(placeCards);
+// list.forEach(addCardListeners);
